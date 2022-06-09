825892b7180493f44918e5d0906f60eeec06df1e
Asefito <asef.farah@usm.cl>
2022-06-08 21:53:47 -0400

    Holaaaaaaaa

--- db_config.php
+++ db_config.php
@@ -1,9 +1,10 @@
 <?php
 /* Detalles de la conexión */
-$conn_string = "host=localhost port=5432 dbname=bname user=postgres password=pasword123";
+$conn_string = "host=localhost port=5432 dbname=tarea2 user=postgres password=mijasef1";
 // Recuerde reemplazar "<contraseña>" por su contraseña y "<nombre_db>" por el nombre de su BD. No se incluyen los "<>".
 // Establecemos una conexión con el servidor postgresSQL
 $dbconn = pg_connect($conn_string);
+
 
 // Revisamos el estado de la conexión en caso de errores.
 if(!$dbconn) {
